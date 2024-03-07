5e63669e735079688e28a00ce597b4076377370c
Kobdias <sofhia.dias@etec.sp.gov.br>
2024-03-07 19:02:25 -0300

    finish

--- Atividade Arnaldo/php/forms.php
+++ Atividade Arnaldo/php/forms.php
@@ -3,17 +3,14 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
+    $tel = $_POST['phone'];
+    $assunto = $_POST['assunto'];
 
     $destinatario = "sofhia.dias@etec.sp.gov.br";
     $assunto = "site devtour";
-    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Message: ".$message;
+    $corpo = "Nome: ".$name."\n"."Email: ".$email."\n"."Telefone: ".$tel."/n"."Assunto: ".$assunto."/n"."Message: ".$message;
 
     $cabecalho = "From: rhuan.figueira@etec.sp.gov.br"."\n"."Replay-to: ".$email."\n"."X=mailer:PHP/".phpversion();
 
-    if(mail($destinatario, $assunto, $corpo, $cabecalho)){
-        echo ("E-mail enviado com sucesso!");
-    } else{
-        echo ("E-mail não foi enviado");
-    }
-
+    echo("Email enviado com sucesso!")
 ?>
